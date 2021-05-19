46c33d55e6fc11132de0469462b9d5e33aea1baf
Alexandre Altgauzen <altgauzen@gmail.com>
2021-05-19 23:01:56 +0100

    Arrumando o repositorio

---
+++ bloco_4/4.1/class/script_oliva_switch.js
@@ -0,0 +1,39 @@
+let weekday = 9;
+
+switch(weekday) {
+  case 1:
+    console.log('Domingo');
+    break;
+  case 2:
+    console.log('Segunda');
+    break;
+  case 3:
+    console.log('Terça-feira');
+    break;
+  case 4:
+    console.log('Quarta-feira');
+    break;
+  case 5:
+    console.log('Quinta-feira');
+    break;
+  case 6:
+    onsole.log('Sexta-feira');
+    break;
+  case 7:
+    console.log('Sábado');
+    break;
+  default:
+    console.log('Não existe esse dia ainda');
+}
+
+
+let age = 19;
+
+switch(age >= 18) {
+  case true:
+    console.log('Pode dirigir');
+    break;
+  case false:
+    console.log('Não pode dirigir');
+    break;
+}
---
+++ bloco_4/4.1/class/scripts_part4_exercise.js
@@ -0,0 +1,10 @@
+let notaCandidato = 75;
+if (notaCandidato >= 80) {
+  console.log("Parabéns, você foi aprovado(a)");
+}
+else if (notaCandidato < 80 && notaCandidato >= 60) {
+  console.log("Você está na nossa lista de espera");
+}
+else {
+  console.log("Você foi reprovado(a)");
+}
