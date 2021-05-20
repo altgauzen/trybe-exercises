d0af48e21f93bc2ae86c1690a21b76b23b77bbb3
Alexandre Altgauzen <altgauzen@gmail.com>
2021-05-20 23:58:54 +0100

    Exercises 6 e 7

--- bloco_4/4.2/class/exercisesAlexandre.js
+++ bloco_4/4.2/class/exercisesAlexandre.js
@@ -4,7 +4,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
     let percorreNumbers = numbers[index];
     console.log(percorreNumbers);
 }
-*/
+
 
 //2-Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
 
@@ -17,13 +17,13 @@ let sum = 0;
 
 /*3-Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
 A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos.
-*/
+
 let media = sum / numbers.length;
 console.log(media);
 
 /*4-Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a 
 mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
-*/
+
 
 if (media > 20) {
   console.log('Valor maior que 20')
@@ -41,3 +41,30 @@ for (let indexM = 0; indexM < numbers.length; indexM += 1) {
 }
   console.log(maiorValor);
 
+//6-Descubra quantos valores ímpares existem no array e imprima o resultado.
+// Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
+
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let quantidadeValoresImpares = 0;
+for (let indexI = 0; indexI < numbers.length; indexI += 1) {
+  if (numbers[indexI] % 2 === 0) {
+  } else {
+    quantidadeValoresImpares += 1;
+  }
+}
+  console.log(quantidadeValoresImpares);
+*/
+
+
+//7-Utilizando for , descubra qual o menor valor contido no array e imprima-o;  
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let menorValor;
+for (let indexMenor = 0; indexMenor < numbers.length; indexMenor += 1) {
+  if (numbers[indexMenor] > menorValor) {
+      } else {
+    menorValor = numbers[indexMenor]
+  }
+}
+  console.log(menorValor);
