167e33083874223b3f1cf7eb2079021101e09259
Alexandre Altgauzen <altgauzen@gmail.com>
2021-05-21 23:04:19 +0100

    Exercicio 1 concluido

---
+++ bloco_4/4.3/exercises.js
@@ -0,0 +1,41 @@
+/* n = 5
+
+*****
+*****
+*****
+*****
+*****
+
+
+// criar uma variavel n > 1 com valor inicial 5
+let n = 5;
+
+// criar uma variavel asterisco
+let asterisco = '*';
+
+let rowSquare
+
+
+for (let index = 0, index )
+*/ 
+
+//imprimir na tela um quadrado feito de asteriscos de lado de tamanho n
+
+let n = 50;
+let line = '';
+if (n >= 1) {
+  for (let index = 0; index < n; index += 1) {
+      for (let index2 = 0; index2 < n; index2 += 1) {
+      line += '*'; 
+      }
+    console.log(line);
+    line = '';
+  }
+} else {
+  console.log('n deve ser maior que 1');
+}
+
+
+
+
+
