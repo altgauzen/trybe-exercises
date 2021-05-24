dbdc59144ec88efcfa9cf5f7ae6f0c10d32949ba
Alexandre Altgauzen <altgauzen@gmail.com>
2021-05-24 19:00:37 +0100

    exercise 3 ainda nao finalizado

--- bloco_4/4.3/exercises.js
+++ bloco_4/4.3/exercises.js
@@ -1,3 +1,4 @@
+/* 1 - imprimir na tela um quadrado feito de asteriscos de lado de tamanho n
 /* n = 5
 
 *****
@@ -5,21 +6,6 @@
 *****
 *****
 *****
-
-
-// criar uma variavel n > 1 com valor inicial 5
-let n = 5;
-
-// criar uma variavel asterisco
-let asterisco = '*';
-
-let rowSquare
-
-
-for (let index = 0, index )
-*/ 
-
-//imprimir na tela um quadrado feito de asteriscos de lado de tamanho n
 
 let n = 50;
 let line = '';
@@ -34,8 +20,58 @@ if (n >= 1) {
 } else {
   console.log('n deve ser maior que 1');
 }
+*/ 
 
 
 
+/* 2 - Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
+n = 5
+
+*
+**
+***
+****
+*****
 
 
+let n = 5;
+let line = '';
+if (n >= 1) {
+  for (let index = 0; index < n; index += 1) {
+      for (let index2 = 0; index2 < index + 1; index2 += 1) {
+      line += '*'; 
+      }
+    console.log(line);
+    line = '';
+  }
+} else {
+  console.log('n deve ser maior que 1');
+}
+*/
+
+/* 3- Agora inverta o lado do triângulo. Por exemplo:
+n = 5
+
+    *
+   **
+  ***
+ ****
+*****
+*/
+
+let n = 5;
+let asterisco = '*';
+let line = ' ';
+let space = ' ';
+if (n >= 1) {
+  for (let index = 0; index < n; index += 1) {
+        for (let index2 = 1; index2 < n - index; index2 += 1) {
+          line = space + asterisco; 
+        }
+    console.log(space, line);
+    space = ' ';
+
+  }
+} else {
+  console.log('n deve ser maior que 1');
+}
